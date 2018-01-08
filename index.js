9fac6d25dc827b0aff1aab88548ad747bfc09535
sarasate <luis@superlumen.io>
2018-01-08 21:52:44 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { Client } = require("dsteem");
+
+const client = new Client("https://api.steemit.com");
+
+const typeDefs = `
+  type Query {
+    account(username: String!): Account 
+  }
+  type Account {
+    name: String! 
+  }
+`;
+
+const resolvers = {
+  Query: {
+    account: async (root, args) => {
+      const res = await client.database.getAccounts([args.username]);
+      return res[0];
+    }
+  }
+};
+
+const options = { port: 4000 };
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(options, () =>
+  console.log("Server is running on localhost:" + options.port)
+);
