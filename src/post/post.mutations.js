2a513556ca627ddbe8e5501e92adb4bb08253909
Luis Höfer <luis@superlumen.io>
2018-01-25 16:03:25 +0100

    Add `createComment` mutation

--- src/post/post.mutations.js
+++ src/post/post.mutations.js
@@ -96,6 +96,67 @@ const mutations = {
     );
 
     return await steem.api.getContentAsync(newPost.author, newPost.permlink);
+  },
+  /**
+   * Convenience method to create a comment with minimal input.
+   * @param root
+   * @param args
+   * @returns {Promise.<*>}
+   */
+  async createComment(root, args) {
+    const { comment, options, key } = args;
+
+    // The Post object
+    const newComment = {
+      author: comment.author,
+      title: "",
+      body: comment.body,
+      permlink: steem.formatter.commentPermlink(
+        comment.parent_author,
+        comment.parent_permlink
+      ),
+      json_metadata: createJSONMetadata({}),
+      parent_author: comment.parent_author,
+      parent_permlink: comment.parent_permlink
+    };
+
+    // Adding beneficiaries by default, 5% to steemql.
+    const beneficiaries = concat(propOr([], "extensions")(options), [
+      {
+        account: "steemql",
+        weight: 500
+      }
+    ]);
+
+    // Optional options
+    const commentOptions = {
+      allow_curation_rewards: pathOr(true, "allow_curation_rewards", options),
+      allow_votes: pathOr(true, "allow_votes", options),
+      author: comment.author,
+      permlink: newComment.permlink,
+      max_accepted_payout: pathOr(
+        "1000000.000 SBD",
+        "max_accepted_payout",
+        options
+      ),
+      // Splitting payout 50/50 between Steem and SBD for now.
+      // `10000` indicates 100% of SBD payout which is a max of 50% of the
+      // total payout.
+      // TODO: Make payout optional.
+      percent_steem_dollars: pathOr(10000, "percent_steem_dollars", options),
+      extensions: [[0, { beneficiaries: beneficiaries }]]
+    };
+
+    const res = await client.broadcast.commentWithOptions(
+      newComment,
+      commentOptions,
+      PrivateKey.from(key)
+    );
+
+    return await steem.api.getContentAsync(
+      newComment.author,
+      newComment.permlink
+    );
   }
 };
 
